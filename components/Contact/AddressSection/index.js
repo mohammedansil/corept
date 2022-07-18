a3a016e2e80cbbd6cd0bbb58adf2363140cfb050
Junaid S <intelpik@gmail.com>
2022-07-18 08:58:32 +0530

    contact map

--- components/Contact/AddressSection/index.js
+++ components/Contact/AddressSection/index.js
@@ -59,11 +59,12 @@ function index() {
         </LeftContainer>
         <RightContainer>
           <Map
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1610340.110288804!2d145.053135!3d-37.972566!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad646b5d2ba4df7%3A0x4045675218ccd90!2sMelbourne%20VIC%2C%20Australia!5e0!3m2!1sen!2sus!4v1657737995037!5m2!1sen!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6161.432483264134!2d50.570572594941765!3d26.21586132811764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49af3f7bbb9283%3A0x103c272ac21821f9!2sBlock%20328%2C%20Manama%2C%20Bahrain!5e1!3m2!1sen!2sin!4v1658114789365!5m2!1sen!2sin"
             allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
           />
+         
         </RightContainer>
       </Container>
     </Section>
