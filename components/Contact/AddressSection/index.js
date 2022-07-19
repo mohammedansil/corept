e1ba3f3d9d6f84799ea13c34614bade0da451def
mohammedansil <anzilmohd4@gmail.com>
2022-07-19 11:05:18 +0530

    map change

--- components/Contact/AddressSection/index.js
+++ components/Contact/AddressSection/index.js
@@ -59,7 +59,7 @@ function index() {
         </LeftContainer>
         <RightContainer>
           <Map
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6161.432483264134!2d50.570572594941765!3d26.21586132811764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49af3f7bbb9283%3A0x103c272ac21821f9!2sBlock%20328%2C%20Manama%2C%20Bahrain!5e1!3m2!1sen!2sin!4v1658114789365!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3582.16509202558!2d50.56970221502846!3d26.126159583472635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x60fc3c618160f353!2zMjbCsDA3JzM0LjIiTiA1MMKwMzQnMTguOCJF!5e0!3m2!1sen!2sbh!4v1658208772614!5m2!1sen!2sbh"
             allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
